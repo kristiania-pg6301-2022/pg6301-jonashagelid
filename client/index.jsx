a99219a38c0fa1981490399e871556e82a11ceb2
JH <jonas.hagelid@gmail.com>
2022-08-10 14:45:10 +0200

    initial setup

---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+ReactDOM.render(<h3>Hello</h3> , document.getElementById("app"));
