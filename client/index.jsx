d3831529d9242add335972378b5bb144b755196e
JH <jonas.hagelid@gmail.com>
2022-08-10 19:38:07 +0200

    Login

--- client/index.jsx
+++ client/index.jsx
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom";
 import {Application} from "./application";
 
 
+export async function fetchJSON(url) {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Failed ${res.status}`);
+    }
+    return await res.json();
+}
+
 ReactDOM.render(<Application /> , document.getElementById("app"));
--- client/loginCallback.jsx
+++ client/loginCallback.jsx
@@ -1,3 +1,22 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 export function LoginCallback() {
-    return null;
+    const navigate = useNavigate();
+
+    useEffect(async()=>{
+        const {access_token} = Object.fromEntries( new URLSearchParams(window.location.hash.substring(1)))
+        console.log(access_token)
+
+        await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({access_token})
+        });
+        navigate("/");
+    })
+
+    return <h1>Loading...</h1>;
 }
